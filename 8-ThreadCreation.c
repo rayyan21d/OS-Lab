01cd9c5a59a9e1de3c8ef7038a602b1dc583dc77
Phoenix2157 <69739582+rayyan21d@users.noreply.github.com>
2023-07-30 01:19:46 +0530

    Update ThreadCreation

--- 8-ThreadCreation.c
+++ 8-ThreadCreation.c
@@ -4,34 +4,30 @@
 #include<pthread.h>
 
 void *runner();
-
 int main(){
-  
+	
 	int i,ret;
-	printf("DEmo of Thread!!\n");
+	printf("Demo of Thread!!\n");
 	pthread_t tid;
 	ret=pthread_create(&tid,NULL,runner,0);
- 
+	
 	if(ret<0){
-  	printf("error in creation of thread");
-  	exit(1);
+	printf("error in creation of thread");
+	exit(1);
 	}
- 
-	pthread_join(tid,0);
-  
+	
 	for(i=0;i<5;i++){
-  	printf("%d inside main\n",i);	
-  	sleep(2);
+	printf("%d inside main\n",i);	
+	sleep(2);
 	}
 	
 	return 0;
 }
 
-
 void *runner(){
 	int i;
 	for(i=0;i<5;i++){
 	printf("%d inside thread\n",i);	
-	sleep(1);}
+	sleep(2);}
 	pthread_exit(0);
 }
